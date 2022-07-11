7ad9e2ad36600b0a19587b30f387e350f4886a7f
Kaio Souza <profkaio@outlook.com>
2022-07-11 19:33:25 -0300

    add ENV

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"go-api/Controllers"
 	"go-api/Helpers"
 	"net/http"
 )
 
 func main() {
+	godotenv.Load()
 	// create routes
 	http.HandleFunc("/api", Controllers.Api)
 	http.HandleFunc("/", Controllers.Home)
